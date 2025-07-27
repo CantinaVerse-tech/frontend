5d591ac08c55ddc768eeda25b4231c17bf2f8a39
cantinaverse <cantinaverse@gmail.com>
2025-07-27 09:47:53 -0400

    Feat: Create ProgressRing.tsx File

---
+++ components/ProgressRing.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function ProgressRing({ 
+    progress = 75, 
+    size = 120, 
+    strokeWidth = 8,
+    label = "Development Progress" 
+}) {}
