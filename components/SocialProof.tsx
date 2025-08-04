f53fcbe102773a3077036215ef38d57880307d41
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-04 19:48:33 -0400

    Feat: Create SocialProof.tsx

---
+++ components/SocialProof.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function SocialProof({ 
+    subscriberCount = 1247,
+    recentUpdates = [],
+    showLiveActivity = true 
+}) {}
