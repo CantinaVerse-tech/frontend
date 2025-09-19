be61b41c127b5497ec51f2dfe2f9fc58f339dbb0
cantinaverse <cantinaverse@gmail.com>
2025-09-19 07:40:47 -0700

    feat: Create NewsletterSignup Component

---
+++ components/NewsletterSignup.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function NewsletterSignup({ 
+    benefits = [
+        "Early access to beta features",
+        "Exclusive educational content",
+        "Priority support",
+        "Community Discord access"
+    ],
+    incentive = "Get a free NFT when you join!" 
+}) {}
